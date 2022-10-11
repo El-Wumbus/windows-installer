7d9ba421a84defc9e328cf0c510034943a6457b8
Decator <decator.c@proton.me>
2022-10-10 22:25:19 -0400

    not done yet

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 #![windows_subsystem = "console"]
 
+use serde_derive::Deserialize;
 use std::{
-    fs::File,
-    io::{read_to_string, BufReader},
+    fs::{create_dir, File, read_dir},
+    io::{BufReader, Read},
     path::{Path, PathBuf},
     process::exit,
 };
-use serde_derive::Deserialize;
 
-#[derive(Debug)]
-#[derive(Deserialize)]
-struct InstallConfiguration {
+#[derive(Debug, Deserialize)]
+struct InstallConfiguration
+{
     install_location: PathBuf,
     set_path: bool,
 }
@@ -21,7 +21,8 @@ fn main()
     let install: PathBuf = resources.join("install.toml");
     let bin: PathBuf = resources.join("bin");
 
-    {// Let's ensure everything is how it should be. If not, exit.
+    {
+        // Let's ensure everything is how it should be. If not, exit.
         if !resources.try_exists().unwrap_or(false)
             || !install.try_exists().unwrap_or(false)
             || !bin.try_exists().unwrap_or(false)
@@ -47,20 +48,84 @@ fn main()
             exit(1);
         }
 
-
-
         if !install.is_file()
         {
-            eprintln!("Expected {} to be a directory!", install.to_string_lossy());
+            eprintln!("Expected {} to be a directory!", install.display());
             exit(1);
         }
     }
 
-    let install_contents = read_to_string(BufReader::new(
+    // Get the config file contents as a String.
+    let mut install_contents = String::new();
+    BufReader::new(
         File::open(install.clone())
-            .expect(format!("Couldn't open file {}", install.to_string_lossy()).as_str()),
-    )).unwrap();
+            .expect(format!("Couldn't open file {}", install.display()).as_str()),
+    )
+    .read_to_string(&mut install_contents)
+    .unwrap();
 
-    let config: InstallConfiguration = toml::from_str(&install_contents).unwrap();
-    println!("CONFIG WORKED: {:?}", config);
+    // Parse the config file into a configuration struct
+    let config: InstallConfiguration = match toml::from_str(&install_contents)
+    {
+        Err(_) =>
+        {
+            eprintln!("Expected a proper config file.");
+            exit(2);
+        }
+        Ok(x) =>
+        {
+            println!("Successfully parsed {}.", install.display());
+            x
+        }
+    };
+
+    // Try to make the target installation location
+    mkdir(config.install_location.clone());
+
+    // Make bin directory
+    mkdir(config.install_location.join("bin"));
+    
+
+    let bin_contents = match read_dir(bin.clone()) {
+        Ok(x) => {
+            println!("Read contents of {}", bin.display());
+            x
+        }
+        Err(x) => {
+            eprintln!("Couldn't read contents of {}: {}", bin.display(), x);
+            exit(3);
+        }
+    };
+    
+    for bin in bin_contents 
+    {
+        match bin {
+            Ok(x) => {
+
+            }
+
+            Err(x) => 
+            {
+                eprintln!();
+                exit(5);
+            }
+        }
+    }
+
+}
+
+fn mkdir(path: PathBuf)
+{
+    match create_dir(path.clone())
+    {
+        Ok(_) => println!(
+            "Created directory: {}",
+            path.display()
+        ),
+        Err(x) =>
+        {
+            eprintln!("Couldn't Create installation directory {}: {}", path.display(), x);
+            exit(3);
+        },
+    }
 }
